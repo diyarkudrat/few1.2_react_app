ac66e30172be586803c241f2322a84dad016b398
Diyar Kudrat <dkudrat@gmail.com>
2021-02-16 12:11:24 -0800

    header component built

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Title.css';
+
+function Title(props) {
+    return (
+        <div className="container">
+            <div className="Title">
+                <h1>SF POPOS</h1>
+                <div className="Title-Subtitle"><h5>San Francisco's Privately Owned Public Spaces</h5></div>
+            </div>
+        </div>
+    );
+}
+
+export default Title;
