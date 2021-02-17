0246b2049bfaf876b2be49053832acf27f6601ca
Diyar Kudrat <dkudrat@gmail.com>
2021-02-17 10:59:13 -0800

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Title from './components/Title';
+import Footer from './components/Footer';
 import POPOSList from './components/POPOSList';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
         <Title />
         <POPOSList />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer(props) {
+    return (
+        <div className="footer">
+            <p>Diyar Kudrat copyright 2021</p>
+        </div>
+    );
+}
+
+export default Footer;
