c0e358391bbc55312bbfb2df6e6eec4830f85537
Diyar Kudrat <dkudrat@gmail.com>
2021-02-17 11:32:52 -0800

    dynamic data content in card

--- src/components/POPOSList.js
+++ src/components/POPOSList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import '../styles/POPOSList.css';
+import data from '../sfpopos-data.json';
 
 function POPOSList(props) {
+    const spaces = data.map(({ title, address, images, hours }) => {
+        if (hours.length === 0) {
+            hours = "Unknown Hours"
+        }
+        
+        return (
+          <POPOSSpace key={title} name={title} address={address} image={images[0]} hours={hours} />
+        )
+    })
+
     return (
         <div className="POPOSList">
-            <POPOSSpace name="50 California Street" address="50 California St." image="50-california-st.jpg"/>
-            <POPOSSpace name="100 Pine Street" address="100 Pine St." image="100-pine.jpg" />
-            <POPOSSpace name="101 California Street" address="101 California St." image="101-california.jpg"/>
-            <POPOSSpace name="Market Street Plaza" address="525 Market St." image="525-market-street-plaza.jpg"/>
-            <POPOSSpace name="Sansome Roof Garden" address="343 Sansome St." image="343-sansome-roof-garden.jpg"/>
-            <POPOSSpace name="Garden Terrace" address="150 California St." image="garden-terrace-at-150-california.jpg" />
+            { spaces }
         </div>
     );
 }
--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -2,12 +2,15 @@ import React from 'react';
 import '../styles/POPOSSpace.css';
 
 function POPOSSpace(props) {
-    const {name, image, address} = props;
+    const { name, image, address, hours } = props;
     return (
         <div className="POPOSSpace">
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
             <h1>{name}</h1>
-            <div className="address">{address}</div>
+            <div className="space-info">
+                <p className="space-address">{address}</p>
+                <p className="space-hours">{hours}</p>
+            </div>
         </div>
     );
 }
