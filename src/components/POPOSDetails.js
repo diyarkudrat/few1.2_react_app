07e771432560340251e0da14b1013137958015e2
Diyar Kudrat <dkudrat@gmail.com>
2021-02-22 11:04:23 -0800

    added styling to details page

--- src/components/POPOSDetails.js
+++ src/components/POPOSDetails.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import data from '../sfpopos-data.json';
+import '../styles/POPOSDetails.css';
 
 function POPOSDetails(props) {
     const { id } = props.match.params;
     const { images, title, desc, hours, features, geo } = data[id];
 
     return (
-        <div>
-          <div>
+        <div className="POPOSDetails">
+          <div className="POPOSDetails-image">
               <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt="header" />
           </div>
 
-          <div>
-              <h1>{ title }</h1>
-              <p>{ desc }</p>
-              <p>{ hours }</p>
-              <p>{ features }</p>
-              <p>{ geo.lat } { geo.lon }</p>
+          <div className="POPOSDetails-info">
+              <h1 className="POPOSDetails-title">{ title }</h1>
+              <p className="POPOSDetails-desc">{ desc }</p>
+              <p className="POPOSDetails-hours">{ hours }</p>
+              <p className="POPOSDetails-features">{ features }</p>
+              <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
           </div>
 
         </div>
