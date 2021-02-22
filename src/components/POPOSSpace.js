ffaa66b0dcfb9b429a597f1e17e5611093911922
Diyar Kudrat <dkudrat@gmail.com>
2021-02-22 10:50:42 -0800

    added styling to list page

--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -6,11 +6,9 @@ function POPOSSpace(props) {
     const { name, image, address, hours, id } = props;
     return (
         <div className="POPOSSpace">
-            <Link to={`/details/${id}`}>
-              <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
-            </Link>
-            <h1><Link to={`/details/${id}`}>{name}</Link></h1>
-            <div className="space-info">
+              <Link className="POPOSSpace" to={`/details/${id}`}><img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" /></Link>
+              <Link className="POPOSSpace-title" to={`/details/${id}`}><h1>{name}</h1></Link>
+            <div className="POPOSSpace-info">
                 <p className="space-address">{address}</p>
                 <p className="space-hours">{hours}</p>
             </div>
