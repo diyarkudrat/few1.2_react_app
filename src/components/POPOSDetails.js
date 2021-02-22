b3d673837701c24dd4531ceb3b84e9a4adc57d0d
Diyar Kudrat <dkudrat@gmail.com>
2021-02-22 13:49:07 -0800

    replaced features for each space with emojis

--- src/components/POPOSDetails.js
+++ src/components/POPOSDetails.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import data from '../sfpopos-data.js';
 import '../styles/POPOSDetails.css';
+import POPOSFeature from './POPOSFeature.js';
+import POPOSFeatureList from './POPOSFeatureList';
 
 function POPOSDetails(props) {
     const { id } = props.match.params;
@@ -16,7 +18,7 @@ function POPOSDetails(props) {
               <h1 className="POPOSDetails-title">{ title }</h1>
               <p className="POPOSDetails-desc">{ desc }</p>
               <p className="POPOSDetails-hours">{ hours }</p>
-              <p className="POPOSDetails-features">{ features }</p>
+              <POPOSFeatureList features={features} />
               <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
           </div>
 
---
+++ src/components/POPOSFeature.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/POPOSFeature.css';
+
+const getFeature = (str) => {
+    switch(str) {
+        case 'outdoors': 
+            return '🌲'
+        case 'coffee':
+            return '☕️'
+        case 'art':
+            return '🖼'
+        case 'toilet':
+            return '🚽'
+        case 'power':
+            return '🔌'
+        default: 
+            return '？'
+    }
+}
+
+function POPOSFeature(props) {
+    const emoji = getFeature(props.name);
+
+    return (
+        <div className="POPOSFeature">
+          {emoji}
+        </div>
+    );
+}
+
+export default POPOSFeature;
